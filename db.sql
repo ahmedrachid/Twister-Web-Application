0e5f83eb1afcfbc984c7f0a131d1fb7741186816
Ahmed Rachid <58266339+ahmedrachid@users.noreply.github.com>
2019-11-28 11:24:41 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `Brunet_Lin`
+-- Base de données :  `Ahmed_Rachid`
 --
 
 -- --------------------------------------------------------
